05a378538562d58325065ff05cf4a347df0169f2
Emil Ingar Hals <emili0210@osloskolen.no>
2017-11-27 12:40:19 +0100

    Added text effect

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 {
-	
+	var text = ["Hei!", "Vi er Kuben Koder", "Akkurat nå holder vi på med web-design", "blablbablba", "Møt opp på biblioteket på mandager og se mer!"]
+
+	var container = document.getElementById("text");
+
+	i = 0
+	j = 0
+
+	var writeEffect = setInterval(function (){
+	  i++
+	    if(i > text[j].length+5){
+	      j++
+				i = 0;
+	  }
+	  container.innerHTML = text[j].slice(0, i);
+	}, 150);
 }
